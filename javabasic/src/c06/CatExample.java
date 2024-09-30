5735c3835c2108fdf149e10325c7780433f16f43
Baejunil <bae110707@naver.com>
2024-09-30 11:52:37 +0900

    .

---
+++ javabasic/src/c06/CatExample.java
@@ -0,0 +1,14 @@
+package c06;
+
+public class CatExample {
+
+	public static void main(String[] args) {
+		Cat c1=new Cat();
+		Cat c2=new Cat();
+		//클래스는 두 가지 용도가 있습니다
+		//하나는 라이브러리용이고
+		//다른 하나는 실해용이다
+		//라이브러리 클래스는 다른 크래스에서 이용할 목적으로 설게됩니다
+		
+	}
+}
---
+++ javabasic/src/c06/MemberOfClass.java
@@ -0,0 +1,19 @@
+package c06;
+
+public class MemberOfClass {
+	//클래스의 구성 맴버
+	//필드(field) -객체의 데이터가 저장되는 곳
+	int fieldname;
+	//생성자-객체 생성 시 초기화 역활당담
+	public MemberOfClass() {
+		
+	}
+	//메소드-객체의 동작에 행당하는 실행 불록
+	public void methodName() {
+		
+	}
+	
+	//클래스에는 객체가 가져야 할 구성 맴버가 선언됩니다
+	//구성 맴버에는 필드.생성자,메소드가 있습니다
+	//이구성맴버들의 생략되거나 복수의 개수로 작성 될수있습니다.
+}
